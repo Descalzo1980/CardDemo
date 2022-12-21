fbb5ae92355a336339bb74247890fc76118136cf
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-21 14:13:35 +0300

    Initial commit

---
+++ app/src/main/java/ru/stas/carddemo/MainActivity.kt
@@ -0,0 +1,33 @@
+package ru.stas.carddemo
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ru.stas.carddemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        layoutManager = LinearLayoutManager(this)
+        binding.contentMain.recyclerView.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        binding.contentMain.recyclerView.adapter = adapter
+    }
+}
---
+++ app/src/main/java/ru/stas/carddemo/Robots.kt
@@ -0,0 +1,9 @@
+package ru.stas.carddemo
+
+data class Robots(
+
+val title: String,
+var image: Int,
+val details: String
+
+)
